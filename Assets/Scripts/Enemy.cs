f49187e7103119c84ed879d230c09a1d21e3ab10
coleyoung02 <coleyoung741@gmail.com>
2024-04-08 13:29:48 -0700

    circle colliders

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Transform left;
+    [SerializeField] private Transform right;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float maxSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        LeftRightMovement();
     }
 
     //needs to be implemented
@@ -21,4 +24,22 @@ public class Player : MonoBehaviour
     {
         return;
     }
+
+    //Done, depending on how you implement future features, you may need to touch it though...
+    private void LeftRightMovement()
+    {
+        float lrMag = 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            lrMag -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            lrMag += 1;
+        }
+        Vector2 v = rb.velocity;
+        v.x = lrMag * maxSpeed;
+        rb.velocity = v;
+    }
+
 }
